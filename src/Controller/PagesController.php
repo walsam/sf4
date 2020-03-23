769b67e2c8ebaebe131f7386cbbc4c1b56dd3ddd
technomaker maarad.walid95 <maarad.walid95@gmail.com>
2020-03-23 11:35:52 +0100

    show the lest 3 products in the homepage with a clickable title to show details.

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -4,6 +4,10 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
+use App\Repository\ProductRepository;
+use App\Entity\Product;
+
+
 
 class PagesController
 {
@@ -15,10 +19,12 @@ class PagesController
     /**
      * @Route("/", name="homepage")
      */
-    public function index()
+    public function index(ProductRepository $productRepository): Response
     {
         return new Response(
-            $this->twig->render('/pages/Homepage.html.twig')
+            $this->twig->render('/pages/Homepage.html.twig',[
+                'products' => $productRepository->getLastThreeProducts(),
+            ])
         );
 
     }
--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -19,32 +19,13 @@ class ProductRepository extends ServiceEntityRepository
         parent::__construct($registry, Product::class);
     }
 
-    // /**
-    //  * @return Product[] Returns an array of Product objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
+    public function getLastThreeProducts(){
+        $query = $this->getEntityManager()->getRepository('App:Product')
+            ->createQueryBuilder('a')
+            ->select('a.id,a.price,a.name,a.quantity,a.description,a.imageURL, a.createAt')
+            ->orderBy('a.id', 'DESC')->getQuery()->setMaxResults(3);
+        $products= $query->getResult();
+        return $products;
 
-    /*
-    public function findOneBySomeField($value): ?Product
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
     }
-    */
 }
